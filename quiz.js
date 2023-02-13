9edb55a910cfe4f600fb9974bfc218d5d83b268b
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-13 17:49:29 -0500

    added startQuiz and renderQuestions functions to quiz.js.  code includes step-by-step comment breakdown of each function

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+// Array of questions and answers for the quiz
 const questions = [
     {
         question: "What is the most watched annual sporting event in the world?",
@@ -48,6 +49,7 @@ const questions = [
 
 ];
 
+// variables from index.html to reference and manipulate DOM elements
 const startButton = document.getElementById("start-button");
 const quizContainer = document.getElementById("quiz-container");
 const quiz = document.getElementById("quiz");
@@ -56,3 +58,72 @@ const resultsContainer = document.getElementById("results-container");
 const finalScore = document.getElementById("final-score");
 const initialsInput = document.getElementById("initials");
 const saveScoreForm = document.querySelector("form");
+
+// initlaize variables for timer and quiz questions
+let currentQuestionIndex = 0;
+let timeLeft = 75;
+
+// startQuiz function is called when the "Start Quiz" button is clicked
+function startQuiz() {
+    // hide the start button and display the quiz container
+    startButton.style.display = "none";
+    quizContainer.style.display = "block";
+
+    // render the first question
+    renderQuestion();
+
+    // set the timer interval to decrement the timeLeft variable by 1 second
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        // update the timer display
+        timer.textContent = `Time: ${timeLeft} seconds`;
+        // if the timeLeft reaches 0, end the quiz
+        if (timeLeft === 0) {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+// renderQuestion function displays the current question and its options
+function renderQuestion() {
+    // clear the quiz container
+    quiz.innerHTML = "";
+
+    // get the current question object
+    const currentQuestion = questions[currentQuestionIndex];
+
+    // create a paragraph element to display the question text
+    const questionText = document.createElement("p");
+    questionText.textContent = currentQuestion.question;
+    quiz.appendChild(questionText);
+
+    // loop through the options array and create a button for each option
+    currentQuestion.options.forEach((option) => {
+        const optionButton = document.createElement("button");
+        optionButton.textContent = option;
+
+        // add an event listener to the option button to handle the user's answer
+        optionButton.addEventListener("click", (event) => {
+            const selectedOption = event.target.textContent;
+            // if the selected option is the correct answer
+            if (selectedOption === currentQuestion.answer) {
+                // increment the currentQuestionIndex and either render the next question or end the quiz
+                currentQuestionIndex++;
+                if (currentQuestionIndex === questions.length) {
+                    endQuiz();
+                } else {
+                    renderQuestion();
+                }
+            } else {
+                // subtract 5 seconds from the timeLeft if the answer is incorrect
+                timeLeft -= 5;
+            }
+        });
+        quiz.appendChild(optionButton);
+    });
+}
+
+
+
+
+
