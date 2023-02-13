cfe6a300bb666e407f6365fb1fed1b22e58395eb
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-13 17:38:21 -0500

    added questions to quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,50 @@
+const questions = [
+    {
+        question: "What is the most watched annual sporting event in the world?",
+        answers: [
+            { text: "Super Bowl", correct: false },
+            { text: "World Cup", correct: true },
+            { text: "Olympics", correct: false },
+            { text: "NBA Finals", correct: false }
+        ]
+    },
+    {
+        question: "Although up for interpretation, who is most commonly known as the greatest basketball player of all time?",
+        answers: [
+            { text: "Michael Jordan", correct: true },
+            { text: "Lebron James", correct: false },
+            { text: "Kobe Bryant", correct: false },
+            { text: "Magic Johnson", correct: false }
+        ]
+    },
+    {
+        question: "Who is the highest scoring New York Ranger of all time?",
+        answers: [
+            { text: "Wayne Gretzky", correct: false },
+            { text: "Mark Messier", correct: false },
+            { text: "Jaromir Jagr", correct: false },
+            { text: "Rod Gilbert", correct: true },
+
+        ]
+    },
+    {
+        question: "Which of the following is not a current NBA team?",
+        answers: [
+            { text: "New York Knicks", correct: false },
+            { text: "Los Angeles Lakers", correct: false },
+            { text: "New Jersey Nets", correct: true },
+            { text: "New Orleans Pelicans", correct: false },
+        ]
+    },
+    {
+        question: "if you bet $100 on a +100 underdog moneyline, how much money would you net (assuming your team wins)?",
+        answers: [
+            { text: "$100", correct: true },
+            { text: "$200", correct: false },
+            { text: "$300", correct: false },
+            { text: "$400", correct: false },
+        ]
+    },
+
+];
 
